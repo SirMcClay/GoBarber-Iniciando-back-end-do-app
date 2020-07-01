957d743e6cce87fee640a5d316bc9b519f55b93d
sirmcclay <mcclay.design@gmail.com>
2020-07-01 20:52:04 -0300

    Models relation

--- src/database/migrations/1593609181410-CreateAppointments.ts
+++ src/database/migrations/1593609181410-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593609181410
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -17,12 +17,10 @@ export default class CreateAppointments1593609181410
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
           {
             name: 'created_at',
